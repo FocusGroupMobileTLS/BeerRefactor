8c6b4f0d3b69319052d8acdb5b0248d855ecf8c3
Quentin Santos <quentin.santos@epitech.eu>
2018-12-16 15:25:44 +0100

    feat(app): add beer randomizer app

---
+++ punkapi.js
@@ -0,0 +1,19 @@
+import base64 from "base-64"
+
+const rootEndPoint = "https://api.punkapi.com/v2"
+
+const headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
+
+export const getRandomBrewdog = () =>
+    fetch(`${rootEndPoint}/beers/random`, { headers }).then(
+        (response) => {
+            if (response.status !== 200)
+                throw new Error(`API answered with status code ${status}`)
+            else {
+                return response.json()
+            }
+        }
+    )
